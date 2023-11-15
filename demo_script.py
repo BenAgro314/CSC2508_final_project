cfcb5e54e9a7bf8475e38d456173575033d671ed
Ben Agro <bagro@waabi.ai>
2023-11-15 02:26:11 +0000

    new demo script

--- demo_script.py
+++ demo_script.py
@@ -18,3 +18,20 @@ video_path = "/home/bagro/videos/"
 model.build_index(video_path)
 video_path, frame = model.retrieve("whale")
 print(video_path, frame)
+
+#%%
+from IPython.display import HTML
+from base64 import b64encode
+import cv2
+
+cap = cv2.VideoCapture(video_path)
+fps = cap.get(cv2.CAP_PROP_FPS)
+mp4 = open(video_path,'rb').read()
+data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+start_time = frame / fps
+end_time = start_time + 5
+HTML(f"""
+<video width=400 controls autoplay>
+      <source src="{data_url}#t={start_time},{end_time}" type="video/mp4">
+</video>
+""")
